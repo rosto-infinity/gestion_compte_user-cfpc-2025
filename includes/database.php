22cddb51e942267833eeb4292aa70ea1dc99356c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-08 09:37:01 +0100

    // 2-Configurer le mode d'erreur pour lancer des exceptions

---
+++ includes/database.php
@@ -0,0 +1,15 @@
+<?php 
+ $servername = "127.0.0.1";
+ $username = "root";
+ $password = "";
+ $database = "gestion_compte_user-cfpc-2025";
+try {
+     $pdo= new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+     // 2-Configurer le mode d'erreur pour lancer des exceptions
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //  echo "<div style='background-color:#3c763d; color:white;'>Connexion à la base de donnée réussie</div>";
+     
+ } catch(PDOException $e) {
+     echo "<div style='color:red;'>La connexion à la base de données a échoué :</div> " . $e->getMessage();
+ }
+ ?>

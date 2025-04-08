4886ea89bd1153ebc52697a7fcf4f3bf4049d92a
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-08 09:37:09 +0100

    //  3-echo "<div style='background-color:#3c763d; color:white;'>Connexion à la base de donnée réussie</div>";

--- includes/database.php
+++ includes/database.php
@@ -7,7 +7,7 @@ try {
      $pdo= new PDO("mysql:host=$servername;dbname=$database", $username, $password);
      // 2-Configurer le mode d'erreur pour lancer des exceptions
      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //  echo "<div style='background-color:#3c763d; color:white;'>Connexion à la base de donnée réussie</div>";
+    //  3-echo "<div style='background-color:#3c763d; color:white;'>Connexion à la base de donnée réussie</div>";
      
  } catch(PDOException $e) {
      echo "<div style='color:red;'>La connexion à la base de données a échoué :</div> " . $e->getMessage();

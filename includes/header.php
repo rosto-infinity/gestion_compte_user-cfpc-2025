e83a85281c4b8ea0be07a8d04fd06a31fd8bc8bf
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-08 09:43:07 +0100

    Gestion des comptes utilisateurs

--- includes/header.php
+++ includes/header.php
@@ -1,50 +1,33 @@
 <?php
 // session_start();
-
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
     <link rel="stylesheet" href="./css/style.css">
-    <title>Manager Accounts Users</title>
+    <title>manager  Accounts users</title>
 </head>
 
-<body class="bg-gray-100">
-    <header class="bg-gray-800 text-white p-4">
+<body>
+    <header>
         <div class="logo">
-            <h2><a href="index.php" class="text-lg font-semibold">Compte utilisateur</a></h2>
+            <h2><a href="index.php">Compte utilisateur</a></h2>
         </div>
         <nav>
-            <ul class="flex space-x-4">
-                <?php if (isset($_SESSION['auth'])) : ?>
-                    <li><a id="gcu" href="logout.php" class="hover:text-gray-300">Se déconnecter</a></li>
-                <?php else : ?>
-                    <li><a href="register.php" class="hover:text-gray-300">S'inscrire</a></li>
-                    <li><a href="login.php" class="hover:text-gray-300">Se connecter</a></li>
-                <?php endif; ?>
+            <ul>
+                 <li><a id="gcu" href="logout.php">Se deconnecter</a></li>
+                <li><a href="register.php">S'inscrire</a></li>
+                <li><a href="login.php">Se connecter</a></li>
             </ul>
         </nav>
-    </header>
-    <main class="p-4">
-        <span>
-            <?php if (isset($_SESSION['flash'])) : ?>
-                <?php foreach ($_SESSION['flash'] as $type => $message) : ?>
-                    <div class="alert alert-<?= $type ?> bg-<?= $type ?>-100 border border-<?= $type ?>-400 text-<?= $type ?>-700 px-4 py-3 rounded relative" role="alert">
-                        <?= $message ?>
-                    </div>
-                <?php endforeach; ?>
-                <?php unset($_SESSION['flash']) ?>
-            <?php endif; ?>
-        </span>
-    </main>
-</body>
 
-</html>
+    </header>
+    <span>
+        <h1>Gestion des comptes utilisateurs</h1>
+    </span>

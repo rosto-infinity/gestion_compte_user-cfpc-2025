ccbd5eccad211756c1034bf05b8bc573777baf98
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-14 16:40:07 +0100

    requette pour recuperer l'utilisateur dans la base de données

--- confirm.php
+++ confirm.php
@@ -5,3 +5,10 @@ require_once('./includes/database.php');
 $userId = $_GET['id'];
 // recuperer le token de l'utilisateur pour lequel on veut confirmer l'inscription
 $token = $_GET['token'];
+
+// requette pour recuperer l'utilisateur dans la base de données
+$query = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+$query->execute([$userId]);
+$user = $query->fetch();
+
+var_dump($user);

7d242eb53e34275c467a1145a1e1062184310b29
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-14 16:37:35 +0100

    // recuperer le token de l'utilisateur pour lequel on veut confirmer l'inscription

--- confirm.php
+++ confirm.php
@@ -3,3 +3,5 @@ session_start();
 require_once('./includes/database.php');
 // recuperer l'id de l'utilisateur pour lequel on veut confirmer l'inscription
 $userId = $_GET['id'];
+// recuperer le token de l'utilisateur pour lequel on veut confirmer l'inscription
+$token = $_GET['token'];
